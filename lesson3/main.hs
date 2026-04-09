53ff1045990b80d3f6994443427ed2eb27a1a348
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-10 00:22:40 +0900

    3-3

--- lesson3/main.hs
+++ lesson3/main.hs
@@ -5,3 +5,27 @@ sumSquareOrSquareSum x y =
   where
     sumSquare = x ^ 2 + y ^ 2
     squareSum = (x + y) ^ 2
+
+sumSquareOrSquareSum' x y =
+  ( \sumSquare squareSum ->
+      if sumSquare > squareSum
+        then sumSquare
+        else squareSum
+  )
+    (x ^ 2 + y ^ 2)
+    ((x + y) ^ 2)
+
+sumSquareOrSquareSum'' x y =
+  let sumSquare = (x ^ 2 + y ^ 2)
+      squareSum = (x + y) ^ 2
+   in if sumSquare > squareSum
+        then sumSquare
+        else squareSum
+
+overwrite x =
+  let x = 2
+   in let x = 3
+       in let x = 4
+           in x
+
+overwrite' x = (\x -> (\x -> (\x -> x) 4) 3) 2
