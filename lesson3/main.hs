9b5260b337c95e2e45174ed824e54ddc253935c7
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-10 23:10:34 +0900

    scope

--- lesson3/main.hs
+++ lesson3/main.hs
@@ -29,3 +29,15 @@ overwrite x =
            in x
 
 overwrite' x = (\x -> (\x -> (\x -> x) 4) 3) 2
+
+x = 4
+
+add1 y = y + x
+
+add2 y = (\x -> y + x) 3
+
+add3 y =
+  ( \y ->
+      (\x -> y + x) 1
+  )
+    2
