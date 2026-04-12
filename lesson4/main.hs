058054d0c3ffb896de1daf0579fc330cde576439
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-12 13:39:57 +0900

    ifEven

---
+++ lesson4/main.hs
@@ -0,0 +1,16 @@
+ifEven myFunction x =
+  if even x
+    then myFunction x
+    else x
+
+inc n = n + 1
+
+double n = n * 2
+
+square n = n ^ 2
+
+ifEvenInc n = ifEven inc n
+
+ifEvenDoubl n = ifEven double n
+
+ifEvenSquiare n = ifEven square n
