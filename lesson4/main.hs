1cdf4bf523bc4b11aeb9158c73fc04855685f5a3
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-13 22:28:03 +0900

    sort

--- lesson4/main.hs
+++ lesson4/main.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 ifEven myFunction x =
   if even x
     then myFunction x
@@ -14,3 +16,16 @@ ifEvenInc n = ifEven inc n
 ifEvenDoubl n = ifEven double n
 
 ifEvenSquiare n = ifEven square n
+
+names = [("Ian", "Curtis"), ("Bernard", "Sumner"), ("Peter", "Hook"), ("Stephen", "Morris")]
+
+compareLastNames name1 name2 =
+  if lastName1 > lastName2
+    then GT
+    else
+      if lastName1 < lastName2
+        then LT
+        else EQ
+  where
+    lastName1 = snd name1
+    lastName2 = snd name2
