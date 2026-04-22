e47b8661690f7cdcfadd6489f1a07b9dbdc338f4
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-22 22:21:13 +0900

    lesson6

--- lesson6/main.hs
+++ lesson6/main.hs
@@ -3,3 +3,17 @@ paExample1 = (!!) "dog"
 paExample2 = ("dog" !!)
 
 paExample3 = (!! 2)
+
+isPalindrome :: String -> Bool
+isPalindrome word = word == reverse word
+
+respond :: String -> String
+respond phrase =
+  if '!' `elem` phrase
+    then "wow!"
+    else "uh.. okey"
+
+assignToGroups :: (Num a, Enum a) => a -> [b] -> [(a, b)]
+assignToGroups n = zip groups
+  where
+    groups = cycle [1 .. n]
