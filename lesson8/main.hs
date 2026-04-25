44a1f4a681f0d3bbafd2d2a21b34177e2c2024e1
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-25 23:03:12 +0900

    myTake

--- lesson8/main.hs
+++ lesson8/main.hs
@@ -1,3 +1,12 @@
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (x : xs) = 1 + myLength xs
+
+myTake :: Int -> [a] -> [a]
+myTake _ [] = []
+myTake 0 _ = []
+myTake n (x : xs) = x : rest
+  where
+    rest = myTake (n - 1) xs
+
+myCycle (first : rest) = first : myCycle (rest ++ [first])
