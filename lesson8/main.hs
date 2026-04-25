4614050691d9d6625de233e657ee55fd33a17fa9
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-26 02:04:53 +0900

    lesson8

--- lesson8/main.hs
+++ lesson8/main.hs
@@ -10,3 +10,20 @@ myTake n (x : xs) = x : rest
     rest = myTake (n - 1) xs
 
 myCycle (first : rest) = first : myCycle (rest ++ [first])
+
+ackermann 0 n = n + 1
+ackermann m 0 = ackermann (m - 1) 1
+ackermann m n = ackermann (m - 1) (ackermann m (n - 1))
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x : xs) = myReverse xs ++ [x]
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fastFib :: Int -> Int -> Int -> Int
+fastFib n1 n2 0 = n1
+fastFib n1 n2 counter = fastFib n2 (n1 + n2) (counter - 1)
