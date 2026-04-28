4d773159d7d5725fd5668c0de6c7d9fba7a3c8e3
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-28 22:53:24 +0900

    9.2

--- lesson9/main.hs
+++ lesson9/main.hs
@@ -7,3 +7,14 @@ mul3ByAll (x : xs) = (3 * x) : mul3ByAll xs
 funcToAll :: (a -> a) -> [a] -> [a]
 funcToAll _ [] = []
 funcToAll f (x : xs) = f x : funcToAll f xs
+
+addAnA :: [String] -> [String]
+addAnA [] = []
+addAnA (x : xs) = ("a " ++ x) : addAnA xs
+
+squareAll :: (Num a) => [a] -> [a]
+squareAll [] = []
+squareAll (x : xs) = x ^ 2 : squareAll xs
+
+myMap f [] = []
+myMap f (x : xs) = (f x) : myMap f xs
