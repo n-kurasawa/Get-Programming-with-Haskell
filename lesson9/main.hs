2f9f533b053a0a44d94910d537dfb7372da2e87a
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-29 21:57:29 +0900

    9.3

--- lesson9/main.hs
+++ lesson9/main.hs
@@ -18,3 +18,17 @@ squareAll (x : xs) = x ^ 2 : squareAll xs
 
 myMap f [] = []
 myMap f (x : xs) = (f x) : myMap f xs
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter test [] = []
+myFilter test (x : xs) =
+  if test x
+    then x : myFilter test xs
+    else myFilter test xs
+
+remove :: (a -> Bool) -> [a] -> [a]
+remove test [] = []
+remove test (x : xs) =
+  if test x
+    then remove test xs
+    else x : remove test xs
