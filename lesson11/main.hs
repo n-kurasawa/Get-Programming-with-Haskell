fce8e92bffda93da8cf41f5618505c7ca07a8b64
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-05-16 21:57:00 +0900

    update

--- lesson11/main.hs
+++ lesson11/main.hs
@@ -6,3 +6,15 @@ halve n = div n 2
 
 printDouble :: Int -> String
 printDouble n = show (n * 2)
+
+ifEven :: (Int -> Int) -> Int -> Int
+ifEven f n =
+  if even n
+    then f n
+    else n
+
+simple :: a -> a
+simple x = x
+
+makeTriple :: a -> b -> c -> (a, b, c)
+makeTriple x y z = (x, y, z)
