18f1ad9dba9a75fb70f6879d256016ed216b05d6
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-05-18 23:34:48 +0900

    lesson12

---
+++ lesson12/main.hs
@@ -0,0 +1,21 @@
+type FirstName = String
+
+type LastName = String
+
+type Age = Int
+
+type Height = Int
+
+type PatientName = (String, String)
+
+firstName :: PatientName -> String
+firstName patient = fst patient
+
+lastName :: PatientName -> String
+lastName patient = snd patient
+
+patientInfo :: FirstName -> LastName -> Age -> Height -> String
+patientInfo fname lname age height = name ++ " " ++ ageHeight
+  where
+    name = lname ++ ", " ++ fname
+    ageHeight = "(" ++ show age ++ "yrs. " ++ show height ++ "in.)"
