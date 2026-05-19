772b01280be874c305a922e222eb3b8f87f10316
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-05-19 23:12:29 +0900

    lesson12

--- lesson12/main.hs
+++ lesson12/main.hs
@@ -14,8 +14,8 @@ firstName patient = fst patient
 lastName :: PatientName -> String
 lastName patient = snd patient
 
-patientInfo :: FirstName -> LastName -> Age -> Height -> String
-patientInfo fname lname age height = name ++ " " ++ ageHeight
+patientInfo :: PatientName -> Age -> Height -> String
+patientInfo (fname, lname) age height = name ++ " " ++ ageHeight
   where
     name = lname ++ ", " ++ fname
     ageHeight = "(" ++ show age ++ "yrs. " ++ show height ++ "in.)"
