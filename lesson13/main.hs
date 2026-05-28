3539e4809aece2948c948bb6b9304298f7fc7ac5
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-05-28 22:48:42 +0900

    13

--- lesson13/main.hs
+++ lesson13/main.hs
@@ -9,3 +9,11 @@ addThenDouble x y = (x + y) * 2
 
 class Describable a where
   describe :: a -> String
+
+data Icecream = Chocolate | Vanilla deriving (Show, Eq, Ord)
+
+cycleSucc :: (Bounded a, Enum a, Eq a) => a -> a
+cycleSucc n =
+  if n == maxBound
+    then minBound
+    else succ n
