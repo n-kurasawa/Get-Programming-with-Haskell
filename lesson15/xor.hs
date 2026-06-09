ceee1c69718736543c912bca586e999b9a5c3ffb
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-06-09 23:11:35 +0900

    15.4

--- lesson15/xor.hs
+++ lesson15/xor.hs
@@ -45,3 +45,26 @@ bitsToInt bits = sum (map (\x -> 2 ^ (snd x)) trueLocations)
 
 bitsToChar :: Bits -> Char
 bitsToChar bits = toEnum (bitsToInt bits)
+
+myPad :: String
+myPad = "Shhhhhh"
+
+myPlainText :: String
+myPlainText = "Haskell"
+
+applyOTP' :: String -> String -> [Bits]
+applyOTP' pad plaintext =
+  map
+    (\pair -> (fst pair) `xor` (snd pair))
+    (zip padBits plaintextBits)
+  where
+    padBits = map charToBits pad
+    plaintextBits = map charToBits plaintext
+
+applyOTP :: String -> String -> String
+applyOTP pad plaintext = map bitsToChar bitList
+  where
+    bitList = applyOTP' pad plaintext
+
+encoderDecoder :: String -> String
+encoderDecoder = applyOTP myPad
