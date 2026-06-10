78fa586e3436a7a623923b7be48cdb519df694c2
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-06-10 22:19:43 +0900

    15

--- lesson15/xor.hs
+++ lesson15/xor.hs
@@ -68,3 +68,81 @@ applyOTP pad plaintext = map bitsToChar bitList
 
 encoderDecoder :: String -> String
 encoderDecoder = applyOTP myPad
+
+class Cipher a where
+  encode :: a -> String -> String
+  decode :: a -> String -> String
+
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
+
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN alphabetSize c = toEnum rotation
+  where
+    halfAlphabet = alphabetSize `div` 2
+    offset = fromEnum c + halfAlphabet
+    rotation = offset `mod` alphabetSize
+
+largestCharNumber :: Int
+largestCharNumber = fromEnum (maxBound :: Char)
+
+rotChar :: Char -> Char
+rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
+  where
+    sizeOfAlphabet = 1 + fromEnum (maxBound :: Char)
+
+fourLetterMessage :: [FourLetterAlphabet]
+fourLetterMessage = [L1, L3, L4, L1, L1, L2]
+
+fourLetterEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterEncoder vals = map rot4l vals
+  where
+    alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+    rot4l = rotN alphaSize
+
+data ThreeLetterAlphabet = Alpha | Beta | Kappa deriving (Show, Enum, Bounded)
+
+threeLetterMessage :: [ThreeLetterAlphabet]
+threeLetterMessage = [Alpha, Alpha, Beta, Alpha, Kappa]
+
+threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterEncoder vals = map rot3l vals
+  where
+    alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+    rot3l = rotN alphaSize
+
+rotNdecoder :: (Bounded a, Enum a) => Int -> a -> a
+rotNdecoder n c = toEnum rotation
+  where
+    halfN = n `div` 2
+    offset =
+      if even n
+        then fromEnum c + halfN
+        else 1 + fromEnum c + halfN
+    rotation = offset `mod` n
+
+rotEncoder :: String -> String
+rotEncoder text = map rotChar text
+  where
+    alphaSize = 1 + fromEnum (maxBound :: Char)
+    rotChar = rotN alphaSize
+
+rotDecoder :: String -> String
+rotDecoder text = map rotCharDecoder text
+  where
+    alphaSize = 1 + fromEnum (maxBound :: Char)
+    rotCharDecoder = rotNdecoder alphaSize
+
+data Rot = Rot
+
+instance Cipher Rot where
+  encode Rot text = rotEncoder text
+  decode Rot text = rotDecoder text
+
+data OneTimePad = OTP String
+
+instance Cipher OneTimePad where
+  encode (OTP pad) text = applyOTP pad text
+  decode (OTP pad) text = applyOTP pad text
+
+myOTP :: OneTimePad
+myOTP = OTP (cycle [minBound .. maxBound])
