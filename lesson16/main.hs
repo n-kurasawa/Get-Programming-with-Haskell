378529594c2c00586fd24708980a559638627297
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-06-13 23:32:06 +0900

    16.2

--- lesson16/main.hs
+++ lesson16/main.hs
@@ -10,3 +10,28 @@ data Book = Book
     year :: Int,
     price :: Double
   }
+
+type FirstName = String
+
+type LastName = String
+
+type MiddleName = String
+
+data Name
+  = Name FirstName LastName
+  | NameWithMiddle FirstName MiddleName LastName
+  | TwoInitialsWithLast Char Char LastName
+  | FirstNameWithTwoInits FirstName Char Char
+
+data Creator = AuthorCreator Author | ArticeCreator Artist
+
+data Author = Author Name
+
+data Artist = Person Name | Band String
+
+hpLovecraft :: Creator
+hpLovecraft =
+  AuthorCreator
+    ( Author
+        (TwoInitialsWithLast 'H' 'P' "Lovecraft")
+    )
