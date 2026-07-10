d54066d87466594915e94621b78397387ce9790b
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-07-10 21:36:19 +0900

    18.1

--- lesson18/main.hs
+++ lesson18/main.hs
@@ -37,3 +37,21 @@ toList (Triple x y z) = [x, y, z]
 
 transform :: (a -> a) -> Triple a -> Triple a
 transform f (Triple x y z) = Triple (f x) (f y) (f z)
+
+data List a = Empty | Cons a (List a) deriving (Show)
+
+builtinEx1 :: [Int]
+builtinEx1 = 1 : 2 : 3 : []
+
+ourListEx1 :: List Int
+ourListEx1 = Cons 1 (Cons 2 (Cons 3 Empty))
+
+builtinEx2 :: [Char]
+builtinEx2 = 'c' : 'a' : 't' : []
+
+ourListEx2 :: List Char
+ourListEx2 = Cons 'c' (Cons 'a' (Cons 't' Empty))
+
+ourMap :: (a -> b) -> List a -> List b
+ourMap _ Empty = Empty
+ourMap func (Cons a rest) = Cons (func a) (ourMap func rest)
